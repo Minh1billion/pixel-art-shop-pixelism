9efe6a33a60c563d39a66a91a2a2aa7445042c37
Minh1Billion <minh01212929979@gmail.com>
2026-02-26 14:49:53 +0700

    add debounce for searching by keyword and navigate on home page buttons

--- client-shop/features/user/hooks/useUsers.ts
+++ client-shop/features/user/hooks/useUsers.ts
@@ -6,9 +6,18 @@ import type { UserListResponse } from "@/features/user/types";
 import type { PageResponse } from "@/features/shared/components/types";
 
 export function useUsers(keyword: string, page: number, size: number) {
+  const [debouncedKeyword, setDebouncedKeyword] = useState(keyword);
   const [data, setData] = useState<PageResponse<UserListResponse> | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedKeyword(keyword);
+    }, 400);
+
+    return () => clearTimeout(timer);
+  }, [keyword]);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -17,18 +26,30 @@ export function useUsers(keyword: string, page: number, size: number) {
       try {
         setLoading(true);
         setError(null);
-        const result = await UserService.getAll(keyword, page, size);
-        if (!controller.signal.aborted) setData(result);
+
+        const result = await UserService.getAll(
+          debouncedKeyword,
+          page,
+          size
+        );
+
+        if (!controller.signal.aborted) {
+          setData(result);
+        }
       } catch (err: any) {
-        if (!controller.signal.aborted) setError(err.message);
+        if (!controller.signal.aborted) {
+          setError(err.message);
+        }
       } finally {
-        if (!controller.signal.aborted) setLoading(false);
+        if (!controller.signal.aborted) {
+          setLoading(false);
+        }
       }
     };
 
     fetch();
     return () => controller.abort();
-  }, [keyword, page, size]);
+  }, [debouncedKeyword, page, size]);
 
   return { data, loading, error };
 }
