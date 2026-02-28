d13023b8d86471e57dad78ddb5c9a437bff6f4bd
Minh1Billion <minh01212929979@gmail.com>
2026-02-28 10:07:27 +0700

    improve perf

--- shop/src/main/java/pixelart/shop/shared/config/RedisConfig.java
+++ shop/src/main/java/pixelart/shop/shared/config/RedisConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,6 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
@@ -30,7 +30,7 @@ public class RedisConfig {
 
     @SuppressWarnings("deprecation")
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
         ObjectMapper objectMapper = JsonMapper.builder()
                 .addModule(new JavaTimeModule())
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
@@ -41,8 +41,7 @@ public class RedisConfig {
                 )
                 .build();
 
-        @SuppressWarnings("deprecation")
-        var serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+        var serializer = new org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer(objectMapper);
 
         RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(5))
