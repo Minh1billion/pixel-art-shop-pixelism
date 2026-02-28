d42565c7c79cb947caf49807d5550141c2ce82cb
Minh1Billion <minh01212929979@gmail.com>
2026-02-28 11:56:43 +0700

    remove caching categories

--- shop/src/main/java/pixelart/shop/features/sprite/dto/SpriteResponse.java
+++ shop/src/main/java/pixelart/shop/features/sprite/dto/SpriteResponse.java
@@ -1,23 +1,47 @@
 package pixelart.shop.features.sprite.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import pixelart.shop.features.category.entity.Category;
 import pixelart.shop.features.sprite.entity.Sprite;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public record SpriteResponse(
-        UUID id,
-        String name,
-        String slug,
-        String imageUrl,
-        List<UUID> categoryIds,
-        List<String> categoryNames,
-        String createdBy,
-        LocalDateTime createdAt
-) {
+@Getter
+public class SpriteResponse {
+
+    private final UUID id;
+    private final String name;
+    private final String slug;
+    private final String imageUrl;
+    private final List<UUID> categoryIds;
+    private final List<String> categoryNames;
+    private final String createdBy;
+    private final LocalDateTime createdAt;
+
+    @JsonCreator
+    public SpriteResponse(
+            @JsonProperty("id") UUID id,
+            @JsonProperty("name") String name,
+            @JsonProperty("slug") String slug,
+            @JsonProperty("imageUrl") String imageUrl,
+            @JsonProperty("categoryIds") List<UUID> categoryIds,
+            @JsonProperty("categoryNames") List<String> categoryNames,
+            @JsonProperty("createdBy") String createdBy,
+            @JsonProperty("createdAt") LocalDateTime createdAt
+    ) {
+        this.id = id;
+        this.name = name;
+        this.slug = slug;
+        this.imageUrl = imageUrl;
+        this.categoryIds = categoryIds;
+        this.categoryNames = categoryNames;
+        this.createdBy = createdBy;
+        this.createdAt = createdAt;
+    }
 
     public static SpriteResponse from(Sprite s) {
         return new SpriteResponse(
@@ -25,10 +49,8 @@ public record SpriteResponse(
                 s.getName(),
                 s.getSlug(),
                 s.getImageUrl(),
-                s.getCategories().stream()
-                        .map(Category::getId).toList(),
-                s.getCategories().stream()
-                        .map(Category::getName).toList(),
+                s.getCategories().stream().map(Category::getId).toList(),
+                s.getCategories().stream().map(Category::getName).toList(),
                 s.getCreatedBy().getNickname(),
                 s.getCreatedAt()
         );
